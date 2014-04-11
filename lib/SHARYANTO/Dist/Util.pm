83d4f463e6b587ece2867aa596690684b3ab1ef5
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-04-11 19:19:55 +0700

    Add function: list_dist_modules

--- lib/SHARYANTO/Dist/Util.pm
+++ lib/SHARYANTO/Dist/Util.pm
@@ -5,11 +5,13 @@ use strict;
 use warnings;
 
 use Config;
+use File::Spec;
 
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(
                        packlist_for
+                       list_dist_modules
                );
 
 # VERSION
@@ -29,6 +31,45 @@ sub packlist_for {
     undef;
 }
 
+sub list_dist_modules {
+    my $mod = shift;
+
+    my $packlist = packlist_for($mod);
+    return () unless $packlist;
+
+    # path structure for .packlist: <libprefix> + <arch> + "auto" +
+    # /Module/Name/ + "/.packlist". we want to get <libprefix>
+    my $libprefix;
+    {
+        my ($vol, $dirs, $name) = File::Spec->splitpath(
+            File::Spec->rel2abs($packlist));
+        my @dirs = File::Spec->splitdir($dirs);
+        for (0..@dirs-2) {
+            if ($dirs[$_] eq $Config{archname} && $dirs[$_+1] eq 'auto') {
+                $libprefix = File::Spec->catpath(
+                    $vol, File::Spec->catdir(@dirs[0..$_-1]));
+                last;
+            }
+        }
+        die "Can't find libprefix for packlist $packlist" unless $libprefix;
+    }
+
+    open my($fh), "<", $packlist or return ();
+    my @mods;
+    while (my $l = <$fh>) {
+        chomp $l;
+        next unless $l =~ /\.pm\z/;
+        $l =~ s/\A\Q$libprefix\E// or next;
+        my @dirs = File::Spec->splitdir($l);
+        shift @dirs; # ""
+        shift @dirs if $dirs[0] eq $Config{archname};
+        $dirs[-1] =~ s/\.pm\z//;
+        push @mods, join("::", @dirs);
+    }
+
+    @mods;
+}
+
 1;
 # ABSTRACT: Dist-related utilities
 
@@ -36,7 +77,11 @@ sub packlist_for {
 
  use SHARYANTO::Dist::Util qw(
      packlist_for
+     list_dist_modules
  );
+
+ say packlist_for("Text::ANSITable"); # sample output: /home/steven/perl5/perlbrew/perls/perl-5.18.2/lib/site_perl/5.18.2/x86_64-linux/auto/Text/ANSITable/.packlist
+ my @mods = list_dist_modules("Text::ANSITable"); # -> ()
 
 
 =head1 DESCRIPTION
@@ -46,8 +91,19 @@ sub packlist_for {
 
 =head2 packlist_for($mod) => STR
 
-Find C<.packlist> file for module C<$mod> (which can be in the form of
+Find C<.packlist> file for installed module C<$mod> (which can be in the form of
 C<Package::SubPkg> or C<Package/SubPkg.pm>). Return undef if none is found.
+
+Depending on the content of C<@INC>, the returned path may be absolute or
+relative.
+
+=head2 packlist_for($mod) => LIST
+
+Given installed module name C<$mod> (which must be the name of the main module
+of its distribution), list all the modules. This is done by first finding the
+C<.packlist> file, then look at all the C<.pm> files listed in the packlist.
+
+Will return empty list if fails to get the packlist.
 
 
 =head1 SEE ALSO
